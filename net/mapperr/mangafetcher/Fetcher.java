311c415302555ee81ce4974268cc147adb7c043f
Michele Angelo Perrotta <michelangeloperrotta@gmail.com>
2013-02-11 21:42:56 +0100

    reset

--- net/mapperr/mangafetcher/Fetcher.java
+++
@@ -1,9 +0,0 @@
-package net.mapperr.mangafetcher;
-
-public interface Fetcher
-{
-	public void fetch();
-	public void fetchVolume(String volume);
-	public void fetchChapter(String volume, String chapter);
-	public void fetchPage(String name, String volume, String chapter, String page);
-}
