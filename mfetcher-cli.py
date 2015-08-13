790e86ff7542dc2be3399634ce2f8e1f6782dcec
perrotta <michele.perrotta@cabel.it>
2015-08-13 14:35:25 +0200

    shift to module, first step

---
+++ mfetcher-cli.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+
+import sys
+import requests
+import json
+import os
+import mfetcher
+
+try:
+   usage = "update, fetch <manga_alias> [chapter]"
+
+
+   if len(sys.argv) > 1:
+       command = sys.argv[1]
+   else:
+      print usage
+      sys.exit(2)
+
+   if len(sys.argv) > 2:
+      manga_name = sys.argv[2]
+
+   if len(sys.argv) > 3:
+      chapter = sys.argv[3]
+
+   if command == "update":
+      mfetcher.update_manga_db()
+      sys.exit(0)
+
+
+   if command == "fetch":
+      mfetcher.fetch(manga_name, chapter)
+      sys.exit(0)
+
+
+#  r = requests.get(base_mangaeden_url + "/0?p=1")
+#  for manga in r.json()["manga"]:
+#    print manga["a"], "\t", manga["i"]
+
+except SystemExit, e:
+   sys.exit(e)
+
+except:
+   e = sys.exc_info()[1]
+   print e
+   sys.exit(1)
--- mfetcher.py
+++ mfetcher.py
@@ -1,107 +1,61 @@
-#! /usr/bin/python
+database_file = "mangadb.json"
+base_mangaeden_url = "https://www.mangaeden.com/api"
+base_images_url = "https://cdn.mangaeden.com/mangasimg"
 
-import sys
-import requests
-import json
-import os
+def update_manga_db():
+    r = requests.get(base_mangaeden_url + "/list/0")
+    f = open(database_file,"w")
+    f.write(r.text)
+    f.close()
 
-try:
-   usage = "update, fetch <manga_alias> [chapter]"
+def fetch(manga_name, chapter):
+    with open(database_file) as dbfile:
+        data = json.load(dbfile)
 
+    manga_id = None
 
-   if len(sys.argv) > 1:
-       command = sys.argv[1]
-   else:
-      print usage
-      sys.exit(2)
-  
-   if len(sys.argv) > 2:
-      manga_name = sys.argv[2]
-
-   if len(sys.argv) > 3:
-      chapter = sys.argv[3]
-  
-   base_mangaeden_url = "https://www.mangaeden.com/api"
-   base_images_url = "https://cdn.mangaeden.com/mangasimg"
-   database_file = "mangadb.json"
-
-
-   if command == "update":
-      r = requests.get(base_mangaeden_url + "/list/0")
-      f = open(database_file,"w")
-      f.write(r.text)
-      f.close()
-      sys.exit(0)
-
-
-   if command == "fetch":
-      
-      if manga_name == None:
-         print usage
-         sys.exit(2)
-
-      with open(database_file) as dbfile:
-         data = json.load(dbfile)
-      
-      manga_id = None
-      
-      for manga in data["manga"]:
-         if manga["a"] == manga_name:
+    for manga in data["manga"]:
+        if manga["a"] == manga_name:
             manga_id = manga["i"]
             break
 
-      if manga_id == None:
-         print "[", manga_name, "] not found"
-         sys.exit(1)
+    if manga_id == None:
+        print "[", manga_name, "] not found"
+        return 1
 
-      r = requests.get(base_mangaeden_url + "/manga/" + manga_id)
-      manga_data = r.json()
-      chapters_data = manga_data["chapters"]
+    r = requests.get(base_mangaeden_url + "/manga/" + manga_id)
+    manga_data = r.json()
+    chapters_data = manga_data["chapters"]
 
-      try:
-         chapter
-      except NameError:
-         chapter = None
+    try:
+        chapter
+    except NameError:
+        chapter = None
 
-      if chapter != None:
-         
-         chapter_id = None
-         
-         for chapter_data in chapters_data:
-            if chapter_data[0] == int(chapter):
-               chapter_id = chapter_data[3]
-               break
-         
-         if chapter_id == None:
-            print "chapter [",chapter,"] not found"
-            sys.exit(1)
+    if chapter != None:
 
-         r = requests.get(base_mangaeden_url + "/chapter/" + chapter_id)
-         images_data = r.json()["images"]
-         directory = manga_name + "__" + chapter
-         if not os.path.exists(directory):
+    chapter_id = None
+
+    for chapter_data in chapters_data:
+    if chapter_data[0] == int(chapter):
+    chapter_id = chapter_data[3]
+    break
+
+    if chapter_id == None:
+        print "chapter [",chapter,"] not found"
+        sys.exit(1)
+
+        r = requests.get(base_mangaeden_url + "/chapter/" + chapter_id)
+        images_data = r.json()["images"]
+        directory = manga_name + "__" + chapter
+        if not os.path.exists(directory):
             os.makedirs(directory)
-         for image_data in images_data:
+        for image_data in images_data:
             image_url = image_data[1]
             image_number = image_data[0]
             r = requests.get(base_images_url + "/" + image_url)
             f = open(directory+"/"+str(image_number)+".jpg", "w")
             f.write(r.content)
             f.close()
-      else:
-         print r.text
-
-      sys.exit(0)
-      
-
-#  r = requests.get(base_mangaeden_url + "/0?p=1")
-#  for manga in r.json()["manga"]:
-#    print manga["a"], "\t", manga["i"]
-
-except SystemExit, e:
-   sys.exit(e)
-
-except:
-   e = sys.exc_info()[1]
-   print e
-   sys.exit(1)
+    else:
+        print r.text
